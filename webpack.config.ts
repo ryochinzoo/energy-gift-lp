e4151148c3e1ef8930eae6c80940b6c0b4895ae6
ryochinzoo <h.ryohei.titech@gmail.com>
2024-07-24 16:15:23 +0200

    update webpack

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,4 +1,22 @@
 module.exports = {
+    rules: [
+        {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] }
+        },
+        // Typescript loader
+        {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ["ts-loader"],
+        },
+        {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+        }
+    ],
     resolve: {
       extensions: ['.tsx', '.ts']
     },
